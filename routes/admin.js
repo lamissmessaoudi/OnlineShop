3438dabc9bcd6b6ef260ba8448c9249b0e7917d7
Lamiss Messaoudi <younglamiss@gmail.com>
2020-10-20 22:13:03 +0100

    adding views and styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form method="POST" action="/product"><input type="text" name="title" ><button type="submit" >send</button></input></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post('/product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/');
-    res.send('<h1>Hello to Product!</h1>');
 });
 
 module.exports = router; 

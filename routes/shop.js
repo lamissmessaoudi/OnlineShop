272e4a6abee0ec7c69f0472a742e62fa9dfb6b35
Lamiss Messaoudi <younglamiss@gmail.com>
2020-10-21 11:04:37 +0100

    using pug as templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
+const adminData = require('./admin');
 
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html')
-    )
+    const products = adminData.products;
+    res.render('shop', { prods: products, title: 'Shop', path: '/' });
+
+
 });
 
 module.exports = router; 
